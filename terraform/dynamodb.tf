5e4ed7c30a6d6258843916b6afa03e02277a6d11
Arvind <66010313+Arv-ind-s@users.noreply.github.com>
2025-12-08 22:31:30 +0530

    Add DynamoDB table resource for predictions

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,20 @@
+resource "aws_dynamodb_table" "predictions" {
+  name           = "${var.dynamodb_table_name}-${var.environment}"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "prediction_id"
+  range_key      = "timestamp"
+  
+  attribute {
+    name = "prediction_id"
+    type = "S"
+  }
+  
+  attribute {
+    name = "timestamp"
+    type = "N"
+  }
+  
+  tags = {
+    Name = "Moderation Predictions"
+  }
+}
