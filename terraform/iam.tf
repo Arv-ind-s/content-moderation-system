8a83e0daa366be7e752694d413c760026dfc080b
Arvind <66010313+Arv-ind-s@users.noreply.github.com>
2025-12-08 22:32:08 +0530

    Create IAM role and policy for Lambda function
    Define IAM role and policy for AWS Lambda with permissions for S3, DynamoDB, and CloudWatch Logs.

---
+++ terraform/iam.tf
@@ -0,0 +1,56 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-lambda-role-${var.environment}"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "${var.project_name}-lambda-policy-${var.environment}"
+  role = aws_iam_role.lambda_role.id
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.model_storage.arn,
+          "${aws_s3_bucket.model_storage.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:Query"
+        ]
+        Resource = aws_dynamodb_table.predictions.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
